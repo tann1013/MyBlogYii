f7c9373944ec0989a85d0b923b4c3700e0167309
tanjian <tanjian@edusoho.com>
2019-10-02 18:40:43 +0800

    s

--- protected/config/main_local.php
+++ protected/config/main_local.php
@@ -33,18 +33,19 @@ return array(
         'cache'=>array(
            'class'=>'CFileCache',
         ),
-		//线上
-		'db'=>array(
-			'connectionString' => 'mysql:host=127.0.0.1;dbname=myblogdb',
-			'emulatePrepare' => true,
-			'enableParamLogging' => true,
-			'enableProfiling'=>true,
-			'username' => 'root',
-            'password' => '123456',
-			'charset' => 'utf8',
-			'tablePrefix' => 'bage_',
-		),
-		
+
+        //本地
+        'db'=>array(
+            'connectionString' => 'mysql:host=127.0.0.1;dbname=myblogdb_local',
+            'emulatePrepare' => true,
+            'enableParamLogging' => true,
+            'enableProfiling'=>true,
+            'username' => 'root',
+            'password' => 'root',
+            'charset' => 'utf8',
+            'tablePrefix' => 'bage_',
+        ),
+
 		'errorHandler'=>array(
             'errorAction'=>'error/index',
         ), 
